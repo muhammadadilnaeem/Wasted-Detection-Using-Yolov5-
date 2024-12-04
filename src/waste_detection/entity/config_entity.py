05e023a5c64000b32559bc1e62b9f7712b9739c2
Muhammad Adil Naeem <162784706+muhammadadilnaeem@users.noreply.github.com>
2024-12-04 16:57:59 +0000

    config entity update

--- src/waste_detection/entity/config_entity.py
+++ src/waste_detection/entity/config_entity.py
@@ -24,26 +24,26 @@ class DataIngestionConfig:
 
     data_download_url: str = DATA_DOWNLOAD_URL  # URL for downloading data
 
-@dataclass
-class DataValidationConfig:
-    # Data class to hold configuration for data validation
-    data_validation_dir: str = os.path.join(
-        training_pipeline_config.artifacts_dir, DATA_VALIDATION_DIR_NAME  # Directory for data validation
-    )
+# @dataclass
+# class DataValidationConfig:
+#     # Data class to hold configuration for data validation
+#     data_validation_dir: str = os.path.join(
+#         training_pipeline_config.artifacts_dir, DATA_VALIDATION_DIR_NAME  # Directory for data validation
+#     )
 
-    valid_status_file_dir: str = os.path.join(data_validation_dir, DATA_VALIDATION_STATUS_FILE)  # Path for validation status file
+#     valid_status_file_dir: str = os.path.join(data_validation_dir, DATA_VALIDATION_STATUS_FILE)  # Path for validation status file
 
-    required_file_list = DATA_VALIDATION_ALL_REQUIRED_FILES  # List of required files for validation
+#     required_file_list = DATA_VALIDATION_ALL_REQUIRED_FILES  # List of required files for validation
 
-@dataclass
-class ModelTrainerConfig:
-    # Data class to hold configuration for model training
-    model_trainer_dir: str = os.path.join(
-        training_pipeline_config.artifacts_dir, MODEL_TRAINER_DIR_NAME  # Directory for model training
-    )
+# @dataclass
+# class ModelTrainerConfig:
+#     # Data class to hold configuration for model training
+#     model_trainer_dir: str = os.path.join(
+#         training_pipeline_config.artifacts_dir, MODEL_TRAINER_DIR_NAME  # Directory for model training
+#     )
 
-    weight_name = MODEL_TRAINER_PRETRAINED_WEIGHT_NAME  # Name of the pre-trained weights file
+#     weight_name = MODEL_TRAINER_PRETRAINED_WEIGHT_NAME  # Name of the pre-trained weights file
 
-    no_epochs = MODEL_TRAINER_NO_EPOCHS  # Number of epochs for training
+#     no_epochs = MODEL_TRAINER_NO_EPOCHS  # Number of epochs for training
 
-    batch_size = MODEL_TRAINER_BATCH_SIZE  # Batch size for training
+#     batch_size = MODEL_TRAINER_BATCH_SIZE  # Batch size for training
