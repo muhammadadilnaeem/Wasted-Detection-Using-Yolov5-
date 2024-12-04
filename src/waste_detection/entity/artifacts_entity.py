462f2891023a700aab536a612a84a8d3afcd7946
Muhammad Adil Naeem <162784706+muhammadadilnaeem@users.noreply.github.com>
2024-12-04 16:58:15 +0000

    artifacts entity update

---
+++ src/waste_detection/entity/artifacts_entity.py
@@ -0,0 +1,36 @@
+
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    """
+    A dataclass to hold information about the data ingestion process.
+    
+    Attributes:
+        data_zip_file_path (str): The file path of the zipped data.
+        feature_store_path (str): The path where the features are stored.
+    """
+    data_zip_file_path: str
+    feature_store_path: str
+
+
+# @dataclass
+# class DataValidationArtifact:
+#     """
+#     A dataclass to represent the output of the data validation process.
+    
+#     Attributes:
+#         validation_status (bool): Status indicating whether the data validation passed or failed.
+#     """
+#     validation_status: bool
+
+
+# @dataclass
+# class ModelTrainerArtifact:
+#     """
+#     A dataclass to hold information about the trained model.
+    
+#     Attributes:
+#         trained_model_file_path (str): The file path where the trained model is saved.
+#     """
+#     trained_model_file_path: str
